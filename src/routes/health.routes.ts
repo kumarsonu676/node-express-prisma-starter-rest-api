a8f5da1e4d5aea39576e57c7bf988d4e1ac64749
Sonu Kumar <kumarsonu676@gmail.com>
2026-02-22 01:16:10 +0530

    feat: format health.routes.ts and add Prettier configuration

--- src/routes/health.routes.ts
+++ src/routes/health.routes.ts
@@ -1,15 +1,13 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import { HealthController } from "../controllers/health.controller";
-import { TYPES } from "../config/ioc.types";
-import container from "../config/ioc.config";
+import { HealthController } from '../controllers/health.controller';
+import { TYPES } from '../config/ioc.types';
+import container from '../config/ioc.config';
 
 const healthRouter = Router();
 
-const healthController = container.get<HealthController>(
-  TYPES.HealthController,
-);
+const healthController = container.get<HealthController>(TYPES.HealthController);
 
-healthRouter.get("/check", healthController.checkHealth);
+healthRouter.get('/check', healthController.checkHealth);
 
 export default healthRouter;
